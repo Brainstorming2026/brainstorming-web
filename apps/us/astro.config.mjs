a594415288067761c0e4410af4df4dfc68028165
Luis Angel Anampa Lavado <u202218664@upc.edu.pe>
2026-08-11 09:06:37 -0500

    feat: Add new components for OAKRs Workshop landing page

---
+++ apps/us/astro.config.mjs
@@ -0,0 +1,19 @@
+import tailwindcss from '@tailwindcss/vite'
+
+import icon from 'astro-icon'
+
+// @ts-check
+import { defineConfig } from 'astro/config'
+
+export default defineConfig({
+  site: 'https://us.brainstorming.la',
+  trailingSlash: 'never',
+  output: 'static',
+
+  prefetch: { prefetchAll: true, defaultStrategy: 'viewport' },
+  integrations: [icon()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+})
